d5ee1385165af5b7a425537d2566335f7c232c49
Calterat <Calterat@gmail.com>
2020-10-22 13:08:56 -0500

    modified final page again before Q and A

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,10 +51,14 @@ const clearQuizWrapperEl = (event) => {
     quizWrapperEl.innerHTML = '';
 }
 
+// Q and A
+
 // Start the Quiz from the Start Button with the count down and calling of Main Sections
 const startQuiz = (event) => {
     countDownEl.textContent = timer;
     restartEl.textContent = "Go back to start page";
+    // call Q and A
+    
     // Timer for the quiz
     const countDown = () => {
         if (timer !== 1) {
@@ -76,15 +80,46 @@ const startQuiz = (event) => {
     clearQuizWrapperEl();
 }
 
-// Finishing Page
+// create Initials Form Element
+
+const initialFormCreate = () => {
+
+    // Create Form element and it's children elements
+    // - form wrapper
+    let formEl = document.createElement("form");
+    formEl.setAttribute("class", "initials");
+    // - label for the input
+    let initialLabelEl = document.createElement("label");
+    initialLabelEl.textContent = "Enter initials: ";
+    initialLabelEl.setAttribute("for", "initialInput");
+    // - text input element for initials
+    let initialInputEl = document.createElement("input");
+    initialInputEl.setAttribute("type", "text");
+    initialInputEl.setAttribute("id", "initialInput");
+    initialInputEl.setAttribute("name", "initialInput");
+    // - button to submit form
+    let initialButtonEl = document.createElement("button");
+    initialButtonEl.textContent = "Submit";
+    initialButtonEl.setAttribute("type", "submit");
+    // -- append all created child elements to the form wrapper
+    formEl.appendChild(initialLabelEl);
+    formEl.appendChild(initialInputEl);
+    formEl.appendChild(initialButtonEl);
+    return formEl;
+}
+
+// Finishing Page Population
 
 const tallyQuizWrapperEl = () => {
-    // populate final screen
-    quizWrapperEl.appendChild(questionEl);
-    quizWrapperEl.appendChild(scoreEl);
-    mainEl.appendChild(quizWrapperEl);
+    // place children elements with filled in text in empty quiz wrapper
     questionEl.textContent = "The quiz has ended!";
     scoreEl.textContent = "Your score: " + score;
+    quizWrapperEl.appendChild(questionEl);
+    quizWrapperEl.appendChild(scoreEl);
+    quizWrapperEl.appendChild(initialFormCreate());
+    // place quizWrapper in main section
+    mainEl.appendChild(quizWrapperEl);
+    // footer announcment
     if (score === 100) {
         correctOrWrongEl.textContent = "Perfect Score!";
     } else if (score >= 70) {
